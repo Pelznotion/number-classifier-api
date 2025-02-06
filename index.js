b6a43af1377f1c1679b89b3088b3bfae285011f5
Adeniyi Adejare <pelznotion@gmail.com>
2025-02-05 15:47:27 -1100

    Fixed input validationand error handling in classify-number endpoint

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 
-const classifyNumber = (num) => {
-  const properties = {
-    isEven: num % 2 === 0,
-    isPrime: isPrime(num),
-    isPerfectSquare: Number.isInteger(Math.sqrt(num)),
-    isCube: Number.isInteger(Math.cbrt(num)),
-    isArmstrong: isArmstrong(num),
-  };
-
-  return properties;
-};
-
 const isPrime = (num) => {
   if (num < 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i++) {
@@ -32,18 +20,20 @@ const isArmstrong = (num) => {
   return sum === num;
 };
 
-// const funFact = [
-//   "Zero is the only number that is neither positive nor negative",
-//   "The number 1 is the only number that is neither prime nor composite",
-//   "Pi is an irrational number and goes on forever without repeating",
-//   "The number 371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371",
-//   "Perfect Squares are numbers that can be formed by multiplying a number by itself",
-// ];
+const classifyNumber = (num) => {
+  return {
+    isEven: num % 2 === 0,
+    isPrime: isPrime(num),
+    isPerfectSquare: Number.isInteger(Math.sqrt(num)),
+    isCube: Number.isInteger(Math.cbrt(num)),
+    isArmstrong: isArmstrong(num),
+  };
+};
 
 app.get("/api/classify-number", async (req, res) => {
   const { number } = req.query;
 
-  if (!number || isNaN(number) || !Number.isInteger(Number(number))) {
+  if (!number || isNaN(number) || parseInt(number) !== Number(number)) {
     return res.status(400).json({
       error:
         "Invalid input. Please provide an integer as the 'number' parameter.",
